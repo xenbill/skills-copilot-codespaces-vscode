e5c9715628b7c5006d84d6e315cddeae26b0cb1d
xenbill <BILLXENAKIS@YAHOO.COM>
2024-11-21 09:29:37 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,23 @@
+//create a web server that listens for requests and serves comments
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+  if(req.url === '/comments.json') {
+    fs.readFile('comments.json', function(err, data) {
+      if(err) {
+        console.error(err);
+        res.writeHead(500, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({error: 'Resource not found'}));
+  }
+}).listen(3000);
+
+console.log('Server listening on port 3000');
